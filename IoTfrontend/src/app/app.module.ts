bbb6a91cb36bd7f7e4145349a3926ba701ae3bb0
RichardSprenger <44767392+RichardSprenger@users.noreply.github.com>
2020-01-16 09:00:43 +0100

    Added Routing and Compontents
    Added all needed components and the routing.
    Updated Prototype

--- IoTfrontend/src/app/app.module.ts
+++ IoTfrontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DetailviewComponent } from './components/detailview/detailview.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AdminviewComponent } from './components/adminview/adminview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DetailviewComponent,
+    SidebarComponent,
+    AdminviewComponent
   ],
   imports: [
     BrowserModule,
