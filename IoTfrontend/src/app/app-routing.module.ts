efc91c604fc8b8ef990bed0ab0dc779aea97fc7f
Prof-JJ <45480494+Prof-JJ@users.noreply.github.com>
2020-01-21 21:52:39 +0100

    routing works + sidbar tests

--- IoTfrontend/src/app/app-routing.module.ts
+++ IoTfrontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SidebarComponent, children: [
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
