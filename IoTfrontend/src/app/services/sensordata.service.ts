778f8e9dbfc4623ab92f8628f53d7d5534193ced
RichardSprenger <44767392+RichardSprenger@users.noreply.github.com>
2020-01-27 08:56:26 +0100

    added sensordata get
    Added the getRequest for sensordata

--- IoTfrontend/src/app/services/sensordata.service.ts
+++ IoTfrontend/src/app/services/sensordata.service.ts
@@ -20,8 +20,32 @@ export class SensordataService {
   };
 
 
-  getSensorData (): Observable <Sensordata[]> {
-    return this.http.get<Sensordata[]('')>
+  getSensorData(id: number): Observable <Sensordata[]> {
+    const url = `${this.sensorDataUrl}/${id}`;
+    return this.http.get<Sensordata[]>(url)
+      .pipe(
+        catchError(this.handleError<Sensordata[]>(`getSensordata id=${id}`))
+      )
+  }
+
+   /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      // this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
